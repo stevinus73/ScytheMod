e96022a2cba0e0329d7a2f6c8fb7b9265e35c88c
Zixuan Wang <wzixuanCC@gmail.com>
2024-07-29 10:30:26 -0400

    callback + reorganizing

--- src/main.js
+++ src/main.js
@@ -1,55 +1,54 @@
-var script=document.querySelector('script[src="https://stevinus73.github.io/IdleCookies/src/main.js"]');
+var script = document.querySelector('script[src="https://stevinus73.github.io/IdleCookies/src/main.js"]');
 /**
  * We are a module!
  */
-script.setAttribute('type','module');
+script.setAttribute('type', 'module');
 
-var LoadModule=function(url,callback,error)
-{
-	var js=document.createElement('script');
+var LoadModule = function (url, callback, error) {
+    var js = document.createElement('script');
     /**
      * Creates a module instead.
      */
-	js.setAttribute('type','module');
-	if (js.readyState){
-		js.onreadystatechange=function()
-		{
-			if (js.readyState==="loaded" || js.readyState==="complete")
-			{
-				js.onreadystatechange=null;
-				if (callback) callback();
-			}
-		};
-	}
-	else if (callback)
-	{
-		js.onload=callback;
-	}
-	if (error) js.onerror=error;
-	
-	js.setAttribute('src',url);
-	document.head.appendChild(js);
+    js.setAttribute('type', 'module');
+    if (js.readyState) {
+        js.onreadystatechange = function () {
+            if (js.readyState === "loaded" || js.readyState === "complete") {
+                js.onreadystatechange = null;
+                if (callback) callback();
+            }
+        };
+    }
+    else if (callback) {
+        js.onload = callback;
+    }
+    if (error) js.onerror = error;
+
+    js.setAttribute('src', url);
+    document.head.appendChild(js);
 }
 LoadModule("https://stevinus73.github.io/IdleCookies/src/engine/engine.js");
 
 var en;
-import("https://stevinus73.github.io/IdleCookies/src/engine/engine.js").then((module) => {
-    en = module.engine;
-}).catch(function(e) {
-    en={};
-    console.error(e);
-});
+var fun = function (mod) {
+    en = mod.engine;
+    en.init();
+}
+var GetModule = function (promise, callback) {
+    promise
+        .then((module) => callback(module))
+        .catch((e) => console.error(e));
+}
 
 var IC = {
-    init: function() {
-        en.init();
+    init: function () {
+        GetModule(import("https://stevinus73.github.io/IdleCookies/src/engine/engine.js"), fun);
     },
 
-    save: function() {
+    save: function () {
         return '';
     },
 
-    load: function(str) {
+    load: function (str) {
 
     },
 };
