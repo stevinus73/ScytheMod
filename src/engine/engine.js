eb94b65ad3c4f63b8827503ef80bbd6b29cc61ed
Zixuan Wang <wzixuanCC@gmail.com>
2024-07-28 22:28:08 -0400

    added shimmer logic added shimmer logic

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -3,3 +3,8 @@ import './constructs.js';
 import './cps.js';
 
 var engine={};
+
+engine.Notify=function(name, subtext, icon, tooltipFunc){
+    Game.Notify(subtext,'<div class="title" style="font-size:18px;margin-top:-2px;">'+name+'</div>',icon);
+	Game.NotifyTooltip(tooltipFunc.toString());
+}
