e7feb1b83a556ded76970fdc8e2e0d26d93bd78e
Zixuan Wang <wzixuanCC@gmail.com>
2024-07-29 20:36:06 -0400

    I hate typos!

--- src/engine/shimmers.js
+++ src/engine/shimmers.js
@@ -75,7 +75,7 @@ shimmer_engine.init = function () {
          */
         //if (me.wrath > 0 && Math.random() < 0.3) list.push('blood frenzy', 'chain cookie', 'cookie storm'); 
 
-        else if (Math.random() < 0.03 && Game.cookiesEarned >= 100000) list.push('chain cookie', 'cookie storm'); 
+        if (Math.random() < 0.03 && Game.cookiesEarned >= 100000) list.push('chain cookie', 'cookie storm'); 
         // if (Math.random() < 0.05 && Game.season == 'fools') list.push('everything must go'); 
         if (Math.random() < 0.1 && (Math.random() < 0.05 || !Game.hasBuff('Dragonflight'))) list.push('click frenzy'); 
         //if (me.wrath && Math.random() < 0.1) list.push('cursed finger'); 
