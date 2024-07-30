cba2031083d047e73600a2f7dfae58e4ffb35235
Zixuan Wang <wzixuanCC@gmail.com>
2024-07-29 20:31:39 -0400

    Adding achievements

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -9,6 +9,14 @@ engine.init=function(){
     shimmer_engine.init();
 }
 
+engine.customAch=function(name,desc,icon,order,shadow) {
+    var ach=new Game.Achievement(name, desc, icon);
+	ach.order=order+ach.id*0.001;
+    if(shadow)Game.last.pool='shadow';
+    LocalizeUpgradesAndAchievs();
+	return ach;
+}
+
 engine.Notify=function(name, subtext, icon){
     Game.Notify(subtext,'<div class="title" style="font-size:18px;margin-top:-2px;">'+name+'</div>',icon);
 }
--- src/main.js
+++ src/main.js
@@ -54,26 +54,21 @@ var Initialize = function (mod) {
     //en.init();
 
     Game.registerHook('create', function() {
-        order=399999;
-        new Game.Achievement('Grail',loc("Perform a Grail combo, having <b>all five distinct golden cookie effects and Elder frenzy</b>.")
-                    +'<q>The holy grail of noscum, one of the most powerful combos in Cookie Clicker. Just pause, and look at how far you\'ve come.</q>',[23,6]);
+        en.customAch('Grail',loc("Perform a Grail combo, having <b>all five distinct golden cookie effects and Elder frenzy</b>.")
+                    +'<q>The holy grail of noscum, one of the most powerful combos in Cookie Clicker. Just pause, and look at how far you\'ve come.</q>',[23,6],399999);
 
-        order=20000;
-        new Game.Achievement('How did we get here?',loc("Have <b>25 golden cookie effects</b> and Elder frenzy.")
-                    +'<q>This is actually probably illegal.</q>',[28,12]);
-        Game.last.pool='shadow';
-        LocalizeUpgradesAndAchievs();
+        en.customAch('How did we get here?',loc("Have <b>every golden cookie effect</b>.")
+                    +'<q>This is actually probably illegal.</q>',[28,12],20000,true);
     });
 
     Game.registerHook('check', function() {
         if (Game.hasBuff('Frenzy') && Game.hasBuff('Dragonflight') 
-            && Game.hasBuff('Dragon Harvest') && Game.hasBuff('Click frenzy') 
-                && Game.hasBuff('Elder frenzy')) {
-            if (buildingSpecials()) {
+            && Game.hasBuff('Dragon Harvest') && Game.hasBuff('Click frenzy')) {
+            if (buildingSpecials() && Game.hasBuff('Elder frenzy')) {
                 Game.Win('Grail');
-                if (buildingSpecials()>=20) {
-                    Game.Win('How did we get here?');
-                }
+            }
+            if (buildingSpecials()>=20) {
+                Game.Win('How did we get here?');
             }
         }
     });
