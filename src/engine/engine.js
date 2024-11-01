70858061572411de4e0382ff043504f17c12194b
Zixuan Wang <wzixuanCC@gmail.com>
2024-11-01 08:41:43 -0400

    weird stuff is happning, I'll try to fix it

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -1,14 +1,114 @@
-import "./shimmers.js";
+
+
+import { shimmer_engine } from "./shimmers.js";
 import "./cps.js";
 import "./constructs.js";
 
-var engine={};
+var IdlersPocket = {};
 
-engine.init=function(){
-    shimmer_engine.init();
+IdlersPocket._Initialize = function () {
+
+
+
+    /**
+     * STORAGE
+     */
+
+    IdlersPocket._save = function() {
+        console.log('testing');
+        return '';
+    }
+
+    IdlersPocket._load = function(str) {
+        console.log('luarping');
+    }
+
+    IdlersPocket.ints=[];
+
+    
+    IdlersPocket._encryptInts = function() {}
+    IdlersPocket._decryptInts = function() {}
+
+    /**
+     * Declares an int variable.
+     * @param {string} name - name of variable
+     */
+    IdlersPocket.intVariable = function (name) {}
+
+    /**
+     * Sets the Int variable.
+     * @param {string} name - name of variable
+     * @param {int} value - value of variable 
+     */
+    IdlersPocket.setInt = function (name, value) {}
+
+    /**
+     * Returns an Int variable corresponding to the name.
+     * @param {string} name - name of variable 
+     */
+    IdlersPocket.getInt = function (name) {}
+
+
+    /**
+     * ACHIEVEMENTS
+     * (work on later)
+     */
+
+    IdlersPocket.Achievements = [];
+    IdlersPocket.NameToId = {};
+    IdlersPocket.Achievement = function (id, name, desc, icon, type, winCon) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.icon = icon;
+        this.type = type;
+        if (winCon) this.winCon = winCon;
+    }
+
+    IdlersPocket.Achievement.prototype.getType = function () {
+        return 'achievement';
+    }
+
+    
+
+    IdlersPocket._CreateAchievement = function (id, name, desc, icon, type, winCon) {
+
+    }
+
+    IdlersPocket._LoadAchievements = function () {
+        var curr;
+        var ach;
+        Object.keys(Game.Achievements).forEach(function (key) {
+            curr = Game.Achievements[key];
+            ach = new this.Achievement(
+                curr.order, curr.name, curr.desc, curr.icon.curr.pool
+            )
+            ach.won = curr.won;
+            this.Achievements.push(ach);
+        });
+        this.Achievements.sort((a, b) => a.id - b.id);
+        this.Achievements.forEach(ach => this.NameToId[ach.name] = ach.id);
+    }
+
+    IdlersPocket.AddAchievement = function (prev, name, desc, icon, winCon) { }
+
+
+    IdlersPocket.Achievement.prototype.pushToFront = function (id, name, desc, icon, type, winCon) { }
 }
 
-engine.Notify=function(name, subtext, icon, tooltipFunc){
-    Game.Notify(subtext,'<div class="title" style="font-size:18px;margin-top:-2px;">'+name+'</div>',icon);
-	Game.NotifyTooltip(tooltipFunc.toString());
+
+
+IdlersPocket.LoadMod = function (name, init) {
+    this._Initialize();
+    var mod = {
+        init: function () {
+            init(this);
+        },
+    
+        save: this._save,
+        load: this._load
+    }
+    Game.registerMod(name, mod);
 }
+
+export { IdlersPocket };
--- src/main.js
+++ src/main.js
@@ -2,17 +2,40 @@ var script=document.querySelector('script[src="https://stevinus73.github.io/Scyt
 script.setAttribute('type','module')
 import("https://stevinus73.github.io/ScytheMod/src/engine/engine.js")
 
-var SM = {
-    init: function() {
-        engine.init();
-    },
+var CreateMod = function (mod) {
+    en = mod.IdlersPocket;
+    en.LoadMod('ScytheMod', function(Id) {
+        console.log("Loaded mod!");
+    });
 
-    save: function() {
-        return '';
-    },
+    // Game.registerHook('create', function() {
+    //     en.customAch('Grail',loc("Perform a Grail combo, having <b>all five distinct golden cookie effects and Elder frenzy</b>.")
+    //                 +'<q>The holy grail of noscum, one of the most powerful combos in Cookie Clicker. Just pause, and look at how far you\'ve come.</q>',[23,6],399999);
 
-    load: function(str) {
+    //     en.customAch('How did we get here?',loc("Have <b>every golden cookie effect</b>.")
+    //                 +'<q>This is actually probably illegal.</q>',[28,12],20000,true);
+    // });
 
-    },
-};
-Game.registerMod("ScytheMod", SM);
+    // Game.registerHook('check', function() {
+    //     if (Game.hasBuff('Frenzy') && Game.hasBuff('Dragonflight') 
+    //         && Game.hasBuff('Dragon Harvest') && Game.hasBuff('Click frenzy')) {
+    //         if (buildingSpecials() && Game.hasBuff('Elder frenzy')) {
+    //             Game.Win('Grail');
+    //         }
+    //         if (buildingSpecials()>=20) {
+    //             Game.Win('How did we get here?');
+    //         }
+    //     }
+    // });
+}
+
+
+
+
+
+var GetModule = function (promise, callback) {
+    promise
+        .then((module) => callback(module))
+        .catch((e) => console.error(e));
+}
+GetModule(import("https://stevinus73.github.io/ScytheMod/src/engine/engine.js"), CreateMod);
