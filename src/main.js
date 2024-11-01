9a0eb43cb337a9bc9a72c537b339d919e90527b7
Steve <wzixuanCC@gmail.com>
2024-11-01 09:36:37 -0400

    trying out something

--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
 var script=document.querySelector('script[src="https://stevinus73.github.io/ScytheMod/src/main.js"]')
 script.setAttribute('type','module')
 
-var CreateMod = function (engine) {
-    engine.IdlersPocket.LoadMod('ScytheMod', function(en) {
-        GetModule(import("https://stevinus73.github.io/ScytheMod/src/logic/buildings.js"), 
-        function(l) {
-            l.mod.load(en);
+var LoadModule = function (url, callback, error) {
+    var js = document.createElement('script');
+    /**
+     * Creates a module instead.
+     */
+    js.setAttribute('type', 'module');
+    if (js.readyState) {
+        js.onreadystatechange = function () {
+            if (js.readyState === "loaded" || js.readyState === "complete") {
+                js.onreadystatechange = null;
+                if (callback) callback();
+            }
+        };
+    }
+    else if (callback) {
+        js.onload = callback;
+    }
+    if (error) js.onerror = error;
+
+    js.setAttribute('src', url);
+    document.head.appendChild(js);
+}
+LoadModule("https://stevinus73.github.io/IdleCookies/src/engine/engine.js");
+
+var CreateMod = function () {
+    IdlersPocket.LoadMod('ScytheMod', function(en) {
+        //GetModule(import("https://stevinus73.github.io/ScytheMod/src/logic/buildings.js"), 
+        LoadModule("https://stevinus73.github.io/ScytheMod/src/logic/buildings.js",
+        function() {
+            mod.load(en);
             console.log("Loaded mod!");
         })
     });
@@ -16,5 +41,5 @@ var GetModule = function (promise, callback) {
         .then((module) => callback(module))
         .catch((e) => console.error(e));
 }
-
-GetModule(import("https://stevinus73.github.io/ScytheMod/src/engine/engine.js"), CreateMod);
+LoadModule("https://stevinus73.github.io/ScytheMod/src/engine/engine.js", CreateMod);
+//GetModule(import("https://stevinus73.github.io/ScytheMod/src/engine/engine.js"), CreateMod);
