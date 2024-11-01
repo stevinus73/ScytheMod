92b9475768659c36fc8864dbb1fd3a3edf395713
Steve <wzixuanCC@gmail.com>
2024-11-01 18:59:21 -0400

    balancing to nerf depletion + a slight cps nerf to buildings + bugfixes

--- src/logic/buildings.js
+++ src/logic/buildings.js
@@ -1,5 +1,11 @@
 var BModify = {}
 
+const baseRhpS_C = 0.5;
+
+function cfl(val) {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
 BModify._Initialize = function(en) {
     this.en = en;
     //Game.UpdateMenu = en.injectCode(Game.UpdateMenu, "(dropMult!=1", `'<div class="listing"><b>'+loc("Missed golden cookies:")+'</b> '+Beautify(Game.missedGoldenClicks)+'</div>' + `, "before")
@@ -12,9 +18,9 @@ BModify._Initialize = function(en) {
         this.rsNames = rsNames;
 
         this.baseYield = this.me.baseCps;
-        this.yield = this.baseYield;
+        this.yield = this.baseYield / baseRhpS_C;
 
-        this.baseRhpS = 1; // resource harvest rate
+        this.baseRhpS = baseRhpS_C; 
         this.RhpS = this.baseRhpS;
 
         this.baseRs = baseRS;
@@ -23,19 +29,19 @@ BModify._Initialize = function(en) {
         this.rsAvailable = baseRS;
 
         this.depleted = false;
+        this.pause = false;
         this.statsView = false;
 
         BModify.rsManagers.push(this);
 
         this.getRawCpS = function() {
-            var cps = this.RhpS * this.yield * this.me.amount;
+            var cps = this.RhpS * this.yield * this.decayedFactor();
             var dmult = 1;
-            if (this.depleted)
+            if (this.depleted || this.pause)
                 dmult = 0;
             return cps * dmult;
         }
 
-        // most important line ever
         this.me.cps = function(me) {
             return me.rsManager.getRawCpS();
         };
@@ -49,7 +55,7 @@ BModify._Initialize = function(en) {
             for (var i in me.tieredUpgrades) {
                 if (!Game.Tiers[me.tieredUpgrades[i].tier].special && Game.Has(me.tieredUpgrades[i].name)) {
                     var tierMult=2; 
-                    var tierRsMult=1.5;
+                    var tierRsMult=math.max(1.5 + 0.1 * i, 2);
                     if (Game.ascensionMode!=1 && Game.Has(me.unshackleUpgrade) && Game.Has(Game.Tiers[me.tieredUpgrades[i].tier].unshackleUpgrade)) {
                         tierMult+=me.id==1?0.5:(20-me.id)*0.1;
                         tierRsMult+=me.id==1?0.25:(20-me.id)*0.05;
@@ -74,6 +80,10 @@ BModify._Initialize = function(en) {
             this.update()
         }
 
+        this.decayedFactor = function() {
+            return this.me.amount * math.pow(0.997, this.me.amount);
+        }
+
         // called once per Game.Logic loop
         this.harvest = function() {
             this.rsAvailable = this.rsTotal - this.rsUsed;
@@ -81,12 +91,20 @@ BModify._Initialize = function(en) {
                 this.depleted = true;
             else
                 this.depleted = false;
+            if (this.pause) {
+                var rate = 0.000003 * Math.max(Math.round((this.rsAvailable/this.rsTotal)*100), 1);
+                this.rsUsed -= (rate / Game.fps) * this.rsTotal;
+                this.rsUsed = math.max(this.rsUsed, 0);
+                return;
+            }
             if (this.depleted) return;
-            this.rsUsed += (this.RhpS / Game.fps) * this.me.amount;
+            this.rsUsed += (this.RhpS / Game.fps) * this.decayedFactor();
         }
 
         l("productMinigameButton"+this.id).insertAdjacentHTML('afterend', 
             '<div id="productStatsButton'+this.id+'" class="productButton" onclick="Game.ObjectsById['+this.id+'].rsManager.switchStats(-1)">View Stats</div>');
+        l("productMinigameButton"+this.id).insertAdjacentHTML('afterend', 
+            '<div id="pauseButton'+this.id+'" class="productButton" onclick="Game.ObjectsById['+this.id+'].rsManager.switch()">Pause</div>');
         l("row"+this.id).insertAdjacentHTML('beforeend', 
             '<div id="rowStats'+this.id+'" style="display: none" class="rowSpecial"></div>'
         )
@@ -96,7 +114,7 @@ BModify._Initialize = function(en) {
         this.getStatDiv = function() { return l("rowStats"+this.id); }
 
         this.getStatDiv().insertAdjacentHTML('beforeend', '<div id="stats'+this.id+'" class="subsection"></div>')
-        l('stats'+this.id).insertAdjacentHTML('beforeend', '<div class="title" style="position:relative">'+this.me.plural+'</div>')
+        l('stats'+this.id).insertAdjacentHTML('beforeend', '<div class="title" style="position:relative">'+cfl(this.me.plural)+'</div>')
         l('stats'+this.id).insertAdjacentHTML('beforeend', '<div id="statsListing'+this.id+'"></div>')
         l('stats'+this.id).insertAdjacentHTML('beforeend', '<div id="statsVisual'+this.id+'"></div>')
 
@@ -128,6 +146,15 @@ BModify._Initialize = function(en) {
                 l('row'+this.id).classList.remove('onMinigame');
             }
         }   
+
+        this.switch = function() {
+            this.pause = !this.pause;
+            if (this.pause) {
+                l('pauseButton'+this.id).textContent = loc("Start");
+            } else {
+                l('pauseButton'+this.id).textContent = loc("Pause");
+            }
+        }
         
         Game.DrawBuildings = BModify.en.injectCode(Game.DrawBuildings, '&& !me.onMinigame ', 
             '&& (me.rsManager ? !me.rsManager.statsView : true) ', "after");
@@ -141,14 +168,13 @@ BModify._Initialize = function(en) {
 
         this.update = function() {
             str = '';
-            var prestigeMult = parseFloat(Game.prestige)*0.01*Game.heavenlyPower*Game.GetHeavenlyMultiplier();
             var sty = this.depleted ? 'style="color:red"' : '';
-            str+='<div class="listing"> <b>'+this.rsNames[0]+' harvest rate ('+this.rsNames[2]+'/second) per '+this.me.dname.toLowerCase()+': </b>'+Beautify(this.RhpS);
-            str+=' ('+Beautify(this.RhpS * this.me.amount)+' for '+Beautify(this.me.amount)+' '+this.me.plural.toLowerCase()+')</div>';
-            str+='<div class="listing"> <b>Yield: </b>'+Beautify(this.yield * prestigeMult)+ " cookies/"+this.rsNames[1]+'</div>';
+            str+='<div class="listing"> <b>'+this.rsNames[0]+' harvest rate ('+this.rsNames[2]+'/second) per '+this.me.dname.toLowerCase()+': </b>'+Beautify(this.RhpS, 1);
+            str+=' ('+Beautify(this.RhpS * this.decayedFactor(), 1)+' for '+Beautify(this.me.amount)+' '+this.me.plural.toLowerCase()+')</div>';
+            str+='<div class="listing"> <b>Base yield: </b>'+Beautify(this.yield, 1)+ " cookies/"+this.rsNames[1]+'</div>';
             str+='<div class="listing"> <b>Total amount of '+this.rsNames[0].toLowerCase()+':</b> '+Beautify(this.rsTotal) + " " + this.rsNames[2]+'</div>';
             str+='<div class="listing"> <b>Harvested '+this.rsNames[0].toLowerCase()+' so far:</b> '+Beautify(this.rsUsed) + " " + this.rsNames[2]+'</div>';
-            str+='<div class="listing" '+sty+'> <b>Total CpS:</b> '+Beautify(this.getRawCpS() * prestigeMult)+" cookies/second"+'</div>';
+            str+='<div class="listing" '+sty+'> <b>Base CpS:</b> '+Beautify(this.getRawCpS(), 1)+" cookies/second"+'</div>';
             l('statsListing'+this.id).innerHTML = str;
         }
 
@@ -158,6 +184,22 @@ BModify._Initialize = function(en) {
 			    this.mbar.style.width='350px';
 		    }
 		    this.mbarFull.style.backgroundPosition=(-Game.T*0.5)+'px';
+        }
+
+        this.clear = function() {
+            this.baseYield = this.me.baseCps;
+            this.yield = this.baseYield / baseRhpS_C;
+            
+            this.baseRhpS = baseRhpS_C; // resource harvest rate
+            this.RhpS = this.baseRhpS;
+
+            this.rsTotal = this.baseRs;
+            this.rsUsed = 0;
+            this.rsAvailable = this.baseRs;
+
+            this.depleted = false;
+            this.pause = false;
+            this.statsView = false;
         }
     }
 
@@ -180,6 +222,9 @@ BModify._Initialize = function(en) {
     Game.registerHook('logic', this.Logic);
     Game.registerHook('check', function() {
         BModify.rsManagers.forEach(mn => mn.update())
+    })
+    Game.registerHook('reincarnate', function() {
+        BModify.rsManagers.forEach(mn => mn.clear())
     })
 
 
