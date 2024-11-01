d64d29be28607c4cb2fa7c0c9c1c28cabfb99400
Zixuan Wang <wzixuanCC@gmail.com>
2024-11-01 08:36:45 -0400

    removing unnecessary code

--- src/main.js
+++ src/main.js
@@ -1,32 +1,6 @@
-var script=document.querySelector('script[src="https://stevinus73.github.io/IdleCookies/src/main.js"]');
-/**
- * We are a module!
- */
-script.setAttribute('type','module');
-
-var LoadModule=function(url,callback,error){
-    var js=document.createElement('script');
-    js.setAttribute('type','module');
-    if (js.readyState){
-	js.onreadystatechange=function() {
-	    if (js.readyState==="loaded" || js.readyState==="complete") {
-		js.onreadystatechange=null;
-		if (callback) callback();
-	    }
-	};
-    }
-    else if (callback) {
-	js.onload=callback;
-    }
-    if (error) js.onerror=error;
-    js.setAttribute('src',url);
-    document.head.appendChild(js);
-}
-
-LoadModule("https://stevinus73.github.io/ScytheMod/src/engine/engine.js");
-import("https://stevinus73.github.io/ScytheMod/src/engine/engine.js").then((module) => {
-    console.log(module);
-});
+var script=document.querySelector('script[src="https://stevinus73.github.io/ScytheMod/src/main.js"]')
+script.setAttribute('type','module')
+import("https://stevinus73.github.io/ScytheMod/src/engine/engine.js")
 
 var SM = {
     init: function() {
