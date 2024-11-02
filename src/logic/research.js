7ea9b2425af938320e8c0713320fe3a270560277
Steve <wzixuanCC@gmail.com>
2024-11-02 15:48:08 -0400

    i am insanely depressed :(

--- src/logic/research.js
+++ src/logic/research.js
@@ -13,13 +13,85 @@ Research._Initialize = function(en) {
     this.button = l("researchButton");
     this.researchOn = false;
     l("centerArea").insertAdjacentHTML('beforeend', 
-        '<style>#research{background: url("img/starbg.jpg"); z-index: 1; position: absolute; inset: 40px 0px 0px;} #researchButton.hover{color:#fff;text-shadow:0px 1px 0px #999,0px 0px 4px #000;}</style>'
+        '<style>#research{background: url("img/starbg.jpg"); z-index: 1; position: absolute; inset: 40px 0px 0px;} #researchButton.hover{color:#fff;text-shadow:0px 1px 0px #999,0px 0px 4px #000; cursor: pointer;}</style>'
     )
-
-    l("centerArea").insertAdjacentHTML('beforeend', 
-        '<div id="research" style="z-index: 1; position: absolute; inset: 40px 0px 0px; "></div>'
-    )
+    l("centerArea").insertAdjacentHTML('beforeend', '<div id="research"></div>')
     this.container = l("research");
+    // this.container.insertAdjacentHTML('beforeend',
+
+    // )
+
+    // this.container.insertAdjacentHTML('beforeend',
+    //     '<canvas id="researchCanvas"></canvas>'
+    // )
+
+    // this.canvas = l("researchCanvas");
+    // this.ctx = this.canvas.getContext("2d");
+
+    this.research = 0;
+
+    this.upgrades = [];
+    this.userX = 0;
+    this.userY = 0;
+    this.id = 0;
+
+    Research.Tech = function(name, desc, priceR, requirements, onBuy, parents, sprite, x, y) {
+        Research.upgrades.push(this);
+        this.name = name;
+        this.desc = desc;
+
+        this.priceR = priceR;
+        this.requirements = requirements;
+        this.onBuy = onBuy;
+        this.parents = parents;
+        this.sprite = sprite;
+        this.x = x; // -1 to 1
+        this.y = y; // -1 to 1
+        this.id = Research.id;
+        this.bought = false;
+
+        this.canBuy = function() {
+            var parentBuy = true;
+            for (var parent in this.parents) {
+                if (!parent.canBuy()) parentBuy = false;
+            }
+            return (this.requirements() && parentBuy && (Research.research >= this.priceR));
+        }
+
+        this.draw = function() {
+            cX = (this.x + 1) * Research.canvas.width * 0.5 - Research.userX;
+            cY = (this.y + 1) * Research.canvas.height * 0.5 - Research.userY;
+            var classes = 'crate upgrade heavenly';
+            var clickStr = this.onBuy;
+            var enabled = 0;
+            if (this.bought) enabled=1;
+            if (enabled) classes += ' enabled';
+            return '<div data-id="'+this.id+'" '+Game.clickStr+'="'+clickStr+'"'+
+            ' class="'+classes+'" '+Game.getDynamicTooltip('function(){return mod.research.upgrades['+this.id+'].getTooltip()}', 'top', true)
+            +'id="researchUp'+this.id+'" '+
+            'style="'+writeIcon(this.sprite)+'position:absolute;left:'+cX+'px;top:'+cY+'px;'+
+                (style||'')+'"></div>';
+        }
+
+        this.getTooltip = function() {
+            return '<div class="description"><div style="margin:6px 0px;font-size:11px;"><b>'+this.name+'</b></div>'+
+						'<div style="margin:6px 0px;font-size:11px;">'+this.desc+'</div>'+
+						""+
+					'</div>';
+        }
+
+        Research.id += 1;
+    }
+
+    Research.Tech.prototype.getType = function () {
+        return 'TechUpgrade';
+    }
+
+
+
+
+
+
 
 
     Research.switch = function(on) {
@@ -35,10 +107,31 @@ Research._Initialize = function(en) {
             this.button.firstChild.textContent = "View Research";
         }
     }
+    this.switch(false);
 
     Research.clear = function() {
         this.switch(false);
     }
+
+    Research.draw = function() {
+        this.canvas.width = this.container.width;
+        this.canvas.height = this.container.height;
+        if (Game.drawT%2==0) {
+            //this.upgrades.forEach(u => u.draw());
+        }
+    }
+
+    f = function(){return true;}
+    new Tech("Magic mushrooms", "They make you magic!", 10, f, f, [], [23, 10], 0, 0);
+
+    str = '';
+    for (var t in this.tech) {
+        str += t.draw();
+    }
+
+    this.container.innerHTML = str;
+
+    //Game.crate(me,'ascend','return;','researchUpgrade','');
 
     Research.en.saveCallback(function() {
 
@@ -47,6 +140,10 @@ Research._Initialize = function(en) {
     Research.en.loadCallback(function() {
 
     })
+
+    Game.registerHook('logic', function() {
+        Research.draw();
+    });
 }
 
 export { Research }
