1b316b089070b6d81558b4ec8ad288da60d1bcbe
Steve <wzixuanCC@gmail.com>
2024-11-09 09:39:14 -0500

    nope?

--- src/logic/buildings.js
+++ src/logic/buildings.js
@@ -362,7 +362,7 @@ BModify._Initialize = function(en) {
                 if (Game.Has(me.grandma.name)) {
                     allocate = '<a class="smallFancyButton" onclick="mod.bModify.grandma.alloc('+i+')" style="width: 70px;">'+loc('Allocate')+'</a>';
                     remove = '<a class="smallFancyButton" onclick="mod.bModify.grandma.remove('+i+')" style="width: 70px;">'+loc('Remove')+'</a>';
-                    str += '<div class="listing">Number of grandmas allocated for ';
+                    str += '<div class="listing"><span style="float: left;>Number of grandmas allocated for </span>';
                     str += ' <span style="float: right;">'+me.plural+': '+allocate + " " + this.grandmaAlloc[i] + " " + remove; 
                     str += '(max: '+this.maxGrandmas()+')</span>';
                     str += '</div>';
