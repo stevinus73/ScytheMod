8a3f45411bc6d6a21db990939f1ff1ac93dcf55e
Steve <wzixuanCC@gmail.com>
2024-12-05 19:16:02 -0500

    microedit: added stats listing and fixed some small errors

--- src/logic/clicks.js
+++ src/logic/clicks.js
@@ -36,18 +36,23 @@ Clicks._Initialize = function(en, Research) {
 
     // show click display
     en.injectCode(Game.Draw, `l('cookies').innerHTML=str;`, 
-        `str=str+'<div style="font-size:50%">(clicks left: '+mod.clicks.clicks+'/'+mod.clicks.maxClicks+')')</div>'\n\t\t`, 
+        `str=str+'<div style="font-size:50%">(clicks left: '+mod.clicks.clicks+'/'+mod.clicks.maxClicks+')'</div>'\n\t\t`, 
         "before");
     
     en.injectMult(Game.ClickCookie, 
         [["|| Game.T<3 ", "|| !mod.clicks.hasClicksLeft() "]
         ,["Game.loseShimmeringVeil('click');", "\n\t\tmod.clicks.drainClick();"]], "after");
+    
+    en.injectCode(Game.UpdateMenu,
+        `'<div class="listing"><b>'+loc("Cookie clicks:")+'</b> '+Beautify(Game.cookieClicks)+'</div>'+`,
+        `\n\t\t'<div class="listing"><b>Cookie clicks left:</b> '+Beautify(mod.clicks.clicks)+'</div>'+`, "after"
+    )
 
     Game.registerHook('cps', function(cps) {
         Clicks.Recalculate();
         return cps;
     })
-    Game.registerHook('logic', this.Logic);
+    Game.registerHook('logic', this.logic);
 }
 
 export { Clicks }
