ddbbf3742ab47fc6b3511b3f05350167b760bb41
Steve <wzixuanCC@gmail.com>
2024-12-22 14:55:03 -0500

    clearing up everything, added the wrath cookie effs

--- src/logic/golden.js
+++ src/logic/golden.js
@@ -1,8 +1,9 @@
 var G = {}
 G._Initialize = function(en, Research) {
     this.me = Game.shimmerTypes.golden;
-    this.maxEffs = 1;
+    this.maxEffs = 3;
 
+    // tweaking around some stuffs lel
     G.me.popFunc = en.injectCode(G.me.popFunc, 
         "buff=Game.gainBuff('click frenzy',Math.ceil(13*effectDurMod),777);",
         "buff=Game.gainBuff('click frenzy',Math.ceil(13*effectDurMod),77*(1+Game.auraMult('Dragon Cursor')*(133/77)));",
@@ -18,20 +19,27 @@ G._Initialize = function(en, Research) {
         "buff=Game.gainBuff('dragon harvest',Math.ceil(60*effectDurMod),25);",
         "replace"
     )
-    const pluralized = {
+
+    // i could do all this with for loops but it just seems like a waste of time and unnecessary abstraction
+    const pluralizedEffs = {
         'frenzy': "Frenzy",
         'dragon harvest': "Dragon Harvest",
         'click frenzy': "Click frenzy",
         'dragonflight': "Dragonflight",
         'blood frenzy': "Elder frenzy",
-        'building special': ""
+        'clot': "Clot",
+        'cursed finger': "Cursed finger",
+        'building special': "",
+        'building debuff': ""
     }
 
     G.isEff = function(choice) {
         return (choice == 'dragonflight' || choice == 'dragon harvest'
             || choice == 'blood frenzy' || choice == 'click frenzy'
             || choice == 'frenzy' || choice == 'building special'
-        ) && !Game.hasBuff(pluralized[choice])
+            || choice == 'building debuff' || choice == 'clot'
+            || choice == 'cursed finger'
+        ) && !Game.hasBuff(pluralizedEffs[choice])
     }
     G.currEffs = function() {
         var effs = [];
@@ -40,8 +48,11 @@ G._Initialize = function(en, Research) {
         if (Game.hasBuff('Click frenzy')) effs.push('Click frenzy');
         if (Game.hasBuff('Elder frenzy')) effs.push('Elder frenzy');
         if (Game.hasBuff('Frenzy')) effs.push('Frenzy');
+        if (Game.hasBuff('Clot')) effs.push('Clot');
+        if (Game.hasBuff('Cursed finger')) effs.push('Cursed finger');
         for (var i in Game.goldenCookieBuildingBuffs) {
-            if (Game.hasBuff(Game.goldenCookieBuildingBuffs[i])) effs.push(Game.goldenCookieBuildingBuffs[i]);
+            if (Game.hasBuff(Game.goldenCookieBuildingBuffs[i][0])) effs.push(Game.goldenCookieBuildingBuffs[i][0]);
+            if (Game.hasBuff(Game.goldenCookieBuildingBuffs[i][1])) effs.push(Game.goldenCookieBuildingBuffs[i][1]);
         }
         return effs;
     }
@@ -49,10 +60,11 @@ G._Initialize = function(en, Research) {
 
     G.me.popFunc = en.injectCode(G.me.popFunc,
         "else if (choice=='blood frenzy')",
-        `else if (choice=='fortune'){var moni=mod.G.fortuneEarn();popup=loc("Fortune!")+'<br><small>'+loc("+%1!",loc("%1 cookie",LBeautify(moni)))+'</small>';}\n\t\t\t`,
+        `else if (choice=='fortune'){var moni=mod.G.fortuneEarn(mult);`
+        +`popup='Fortune!<br><small>Found '+loc("+%1!",loc("%1 cookie",LBeautify(moni)))+'</small>';}\n\t\t\t`,
         "before"
     )
-    G.fortuneEarn = function() {
+    G.fortuneEarn = function(mult) {
         var time=Game.cookiesPs*60*10;
         var moni=mult*time+777;
 		Game.Earn(moni);
