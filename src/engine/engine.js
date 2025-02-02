04637b5c9d07a2c459256a65ed59fc0c42f0b02d
Steve <wzixuanCC@gmail.com>
2025-02-02 12:31:59 -0500

    part 2 of trying to make saving work

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -217,7 +217,7 @@ IdlersPocket.LoadMod = function (name, initFunc) {
                     <div class="line"></div>
                     Credits: Hellranger for testing the mod, yeetdragon24 for 'helping' me make this prompt
                 </div>`,
-            [['Continue','scytheModWrapper.switchSave();scytheModWrapper.initMod();Game.ClosePrompt();'], ['No thanks','scytheModWrapper.unloadMod();Game.ClosePrompt();']]);
+            [['Continue','scytheModWrapper.switchSave();scytheModWrapper.initMod();Game.ModLoaded=true;Game.ClosePrompt();'], ['No thanks','scytheModWrapper.unloadMod();Game.ClosePrompt();']]);
         }
     }
     Game.registerMod(name, mod);
