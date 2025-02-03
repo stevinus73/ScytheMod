25da40f59b9c798241b0f3f2924a6c9d96e26abe
Steve <wzixuanCC@gmail.com>
2025-02-03 13:17:23 -0500

    whoops

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -172,7 +172,7 @@ IdlersPocket._Initialize = function () {
     }
 
     // patched orteil bs
-    eval('Game.CalculateGains='+Game.calculateGains.toString().replace(
+    eval('Game.CalculateGains='+Game.CalculateGains.toString().replace(
         "Game.cookiesPs=Game.runModHookOnValue('cps',Game.cookiesPs);","mult*=Game.runModHookOnValue('cps',Game.cookiesPs)/Game.cookiesPs;"+
         "Game.cookiesPs=Game.runModHookOnValue('cps',Game.cookiesPs);"));
 }
